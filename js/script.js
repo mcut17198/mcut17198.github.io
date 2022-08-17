61a35290a570e6999765de3ee98ffc117b9439f4
Carter Zhang <mcut17198@mcut17198-PC.localhost>
2022-08-17 10:38:00 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":960},"pages":[{"id":"page-carter-zhang-s-homepage","displayName":"Carter Zhang's Homepage","link":{"linkType":"LinkTypePage","href":"#!page-carter-zhang-s-homepage"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
