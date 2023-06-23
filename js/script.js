bf4a475717e637611782b3d13836c702bbdf96c5
Carter Zhang <111365291+mcut17198@users.noreply.github.com>
2023-06-23 08:49:55 +0800

    20230623

--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":960},"pages":[{"id":"page-carter-zhang-s-homepage","displayName":"Carter Zhang's Homepage","link":{"linkType":"LinkTypePage","href":"#!page-carter-zhang-s-homepage"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
